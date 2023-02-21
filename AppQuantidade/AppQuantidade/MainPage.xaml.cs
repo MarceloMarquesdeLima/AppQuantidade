6efe1d0eeaa3131a5b0c1070419457c54fe1868d
Marcelo Marques de Lima <marcelomarquesdelima@Mac-mini-de-Marcelo.local>
2023-02-21 20:45:41 -0300

    Logica para operação

--- AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -8,11 +8,28 @@ using Xamarin.Forms;
 
 namespace AppQuantidade
 {
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        int quantidade = 0;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void Aumentar(System.Object sender, System.EventArgs e)
+        {
+            quantidade++;
+            lblQuantidade.Text = quantidade.ToString();
+        }
+
+        void Diminuir(System.Object sender, System.EventArgs e)
+        {
+            if (quantidade != 0)
+            {
+                quantidade--;
+                lblQuantidade.Text = quantidade.ToString();
+            }
         }
     }
 }
